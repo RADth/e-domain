7b4cb8bbb0c151ec6dbcfd21a520438255b88421
Tharusha <tharushadgunawardane@gmail.com>
2021-11-08 21:05:34 +0530

    Change sqlite to postgres

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'bekjrnlskvOLvR'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
